bc4a2c0faffeaec38a9928c8cca59fb9ab64595a
LautaroLeall <lautaroleal4@gmail.com>
2025-06-11 13:55:45 -0300

    Login - Register - Footer - Navbar listos

---
+++ scripts/cards.js
@@ -0,0 +1,78 @@
+// CARDS
+
+// Accedemos al elemento que contiene el contenido de las Cards
+const cards = document.getElementById("cards");
+
+// Creamos un array con los datos de las cartas
+const myProducts = [
+    {
+        id: 1,
+        nombre: "Auriculares Inalambricos Gtheos",
+        precio: 500,
+        stock: 8,
+        descripcion: "2.4 Ghz Para Pc, Ps4, Ps5",
+        img: "assets/auricular.png"
+    },
+    {
+        id: 2,
+        nombre: "Mouse Recargable Slim Inalámbrico",
+        precio: 1000,
+        stock: 3,
+        descripcion: "2.4ghz Wireless Mac Pc Lin",
+        img: "assets/mouseInalambrico.png"
+    },
+    {
+        id: 3,
+        nombre: "Kit Teclado + Mouse Inalambrico Noga",
+        precio: 2500,
+        stock: 16,
+        descripcion: "S5700 Pc Notebook Usb Verde",
+        img: "assets/kitInalambrico.png"
+    },
+    {
+        id: 4,
+        nombre: "Kit Teclado + Mouse Inalambrico Noga",
+        precio: 120,
+        stock: 8,
+        descripcion: "S5700 Pc Notebook Usb Verde",
+        img: "assets/kitInalambrico.png"
+    },
+    {
+        id: 5,
+        nombre: "Kit Teclado + Mouse Inalambrico Noga",
+        precio: 500,
+        stock: 6,
+        descripcion: "S5700 Pc Notebook Usb Verde",
+        img: "assets/kitInalambrico.png"
+    },
+    {
+        id: 6,
+        nombre: "Kit Teclado + Mouse Inalambrico Noga",
+        precio: 365,
+        stock: 1,
+        descripcion: "S5700 Pc Notebook Usb Verde",
+        img: "assets/kitInalambrico.png"
+    },
+]
+
+// Incrementamos un navbar de bootstrap a nuestro documento
+myProducts.map((product) => {
+    cards.innerHTML += `
+    <div class="card p-1 d-flex flex-column" style="width: 23rem; height: 25rem;">
+        <img src="${product.img}" class="card-img-top" style="height: 210px; object-fit: contain;" alt="Imagen del Producto">
+        <div class="card-body d-flex flex-column justify-content-between flex-grow-1">
+            <div>
+                <h6 class="card-title">${product.nombre}</h6>
+                <p class="card-text">${product.descripcion}</p>
+                <div class="d-flex justify-content-around">
+                    <p class="card-text text-success">$${product.precio}</p>
+                    <p class="card-text text-danger">${product.stock} Disponible</p>
+                </div>
+            </div>
+            <div class="mt-auto d-flex justify-content-center">
+                <a href="#" class="btn btn-outline-secondary w-75">Comprar</a>
+            </div>
+        </div>
+    </div>
+    `;
+});
---
+++ scripts/footer.js
@@ -0,0 +1,46 @@
+// FOOTER
+
+// Accedemos al elemento que contiene el contenido de la footer
+const footer = document.getElementById("footer");
+
+// Incrementamos un navbar de bootstrap a nuestro documento
+footer.innerHTML = `
+    <div class="container-fluid bg-dark py-3 text-center mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4 col-xl-3 mb-3 text-start">
+                <h5 class="text-secondary fw-bold">Contacto</h5>
+                <p class="text-secondary">
+                    <i class="bi bi-geo-alt-fill"></i> San Miguel de Tucumán, Argentina
+                </p>
+                <p class="text-secondary">
+                    <i class="bi bi-envelope-fill"></i> lautaroleal4@gmail.com
+                </p>
+                <p class="text-secondary">
+                    <i class="bi bi-phone"></i> 381 339 9463
+                </p>
+            </div>
+            <div class="col-md-6 col-lg-4 col-xl-3 mb-3">
+                <h5 class="text-secondary fw-bold mb-3">Lautaro Leal</h5>
+                <img src="/assets/lldp-footer.png" class="img-fluid img-footer rounded-circle" alt="Lautaro Leal">
+            </div>
+            <div class="col-md-6 col-lg-4 col-xl-3 mb-3 text-end">
+                <h5 class="text-secondary fw-bold">Redes Sociales</h5>
+                <p>
+                    <a href="https://github.com/LautaroLeall" class="link-redes text-decoration-none text-secondary" target="_blank">
+                        <i class="bi bi-github link-redes"></i> Github
+                    </a>
+                </p>
+                <p>
+                    <a href="https://www.linkedin.com/in/lauldp/" class="link-redes text-decoration-none text-secondary" target="_blank">
+                        <i class="bi bi-linkedin link-redes"></i> Linkedin
+                    </a>
+                </p>
+                <p>
+                    <a href="https://www.instagram.com/lautaro_leall/" class="link-redes text-decoration-none text-secondary" target="_blank">
+                        <i class="bi bi-instagram link-redes"></i> Instagram
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+`;
