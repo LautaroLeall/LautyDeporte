d44d5c2bcee7c3aaa97689eff2721c5c5197222b
LautaroLeall <lautaroleal4@gmail.com>
2025-07-30 18:54:09 -0300

    Modal de pagar - Transferencia/Tarjeta

--- scripts/pay/formTransferencia.js
+++ scripts/pay/formTransferencia.js
@@ -4,9 +4,9 @@ export function generarFormularioTransferencia(formContainer, procesarPago) {
     formContainer.innerHTML = `
         <div class="bg-secondary bg-opacity-10 p-4 rounded-3 border border-secondary">
             <h6 class="text-info fw-bold mb-3">Datos para Transferencia Bancaria:</h6>
-            <p>CBU: <code>1234567890123456789012</code></p>
-            <p>Alias: <code>lautygod.store</code></p>
-            <p>Titular: Lautaro God Developer</p>
+            <p>CBU: <code>4530000800019268948890</code></p>
+            <p>Alias: <code>LAUTARO2.NX</code></p>
+            <p>Titular: Lautaro Leal Del Prete</p>
             <div class="mt-3">
                 <label class="form-label">Adjuntar comprobante (simulado):</label>
                 <input type="file" id="comprobante" class="form-control bg-dark text-light border-secondary">
