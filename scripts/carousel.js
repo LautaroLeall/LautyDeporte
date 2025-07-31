aed4253555c175e0682d1a639d59547acaf950a7
LautaroLeall <lautaroleal4@gmail.com>
2025-07-31 11:50:06 -0300

    Responsive Carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -9,13 +9,13 @@ carousel.innerHTML += `
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="500">
-                <img src="assets/carousel/carousel-1.png" class="d-block w-100" alt="...">
+                <img src="assets/carousel/carousel-1.png" class="img-carousel d-block w-100" alt="...">
             </div>
             <div class="carousel-item" data-bs-interval="2000">
-                <img src="assets/carousel/carousel-2.png" class="d-block w-100" alt="...">
+                <img src="assets/carousel/carousel-2.png" class="img-carousel d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="assets/carousel/carousel-3.png" class="d-block w-100" alt="...">
+                <img src="assets/carousel/carousel-3.png" class="img-carousel d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
